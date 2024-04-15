fdceff18eec9d290c32fdfb1dc4182a64baef768
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-04-15 08:47:34 +0700

    add some view and text

--- App.js
+++ App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.boldText}>Welcome to Header!</Text>
+      </View>
+      <View style={styles.Body}>
+        <Text style={styles.boldText}>Welcome to Body!</Text>
+        <Text style={styles.BodyText}> lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum </Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +22,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header:{
+    backgroundColor: 'blue',
+    height: 50,
+    width: '100%',
+  },
+  boldText:{
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  Body:{
+    backgroundColor:'red',
+    height: 100,
+    width: '100%',
+  },
+  BodyText:{
+    color: 'white',
+    fontSize: 10,
+    textAlign: 'center',
+  }
 });
