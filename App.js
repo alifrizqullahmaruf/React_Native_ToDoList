7d38aacb4086da4db7612fdb75c5c265270b552d
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-04-21 08:54:13 +0700

    React Native Tutorial #8 - Touchable Components

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View, ScrollView, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  // Object atau data yang akan digunakan untuk useState
   const [people, setPeople] = useState([
     { name: 'shaun', key: '1' },
     { name: 'yoshi', key: '2' },
@@ -13,56 +12,69 @@ export default function App() {
     { name: 'bowser', key: '7' },
   ]);
 
+  const handleClick = (id) => {
+    //? Log `id` ke konsol
+    console.log(id);
+  
+    //? Menggunakan setPeople untuk memperbarui state `people`
+    setPeople(prevPeople => 
+      // Mengembalikan array baru dengan menggunakan filter untuk menyaring elemen yang memiliki `key` tidak sama dengan `id` yang diterima
+      prevPeople.filter(person => person.key !== id)
+    );
+  };
+  
+
   return (
     <View style={styles.container}>
       <View style={styles.inputContainer}>
-        <Text style={styles.headline}>List and Scroll View</Text>
+        <Text style={styles.headline}>Touchable Components</Text>
       </View>
 
       <FlatList
+        numColumns={2}
         style={styles.listContainer}
         keyExtractor={(item) => item.key}
         data={people}
         renderItem={({ item }) => (
-          <View style={styles.item}>
+          <TouchableOpacity style={styles.item} onPress={() => handleClick(item.key)}>
             <Text style={styles.text}>{item.name}</Text>
-          </View>
+          </TouchableOpacity>
         )}
       />
-
     </View>
   );
 }
 
-// Style pada tampilan
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    margin: 16,
+    paddingHorizontal: 16,
+    paddingTop: 20,
   },
   headline: {
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
-    margin: 16,
+    marginBottom: 16,
   },
   inputContainer: {
-    margin: 16,
+    marginBottom: 16,
   },
   listContainer: {
     marginTop: 16,
   },
   item: {
-    marginTop: 16,
+    flex: 1,
+    margin: 8,
     padding: 20,
     backgroundColor: 'aqua',
     borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   text: {
     fontSize: 18,
-    fontSize:24,
-    padding: 32,
-    
+    textAlign: 'center',
   },
 });
