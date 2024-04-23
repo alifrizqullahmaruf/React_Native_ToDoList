8451ca0a0591f71f41125b59c1c22aed1be66982
Alif Rizqullah Maruf <alifrizqullahmaruf2003@mail.ugm.ac.id>
2024-04-23 12:40:53 +0700

    add some header

--- App.js
+++ App.js
@@ -1,46 +1,31 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import Header from "./components/hearder";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'shaun', key: '1' },
-    { name: 'yoshi', key: '2' },
-    { name: 'mario', key: '3' },
-    { name: 'luigi', key: '4' },
-    { name: 'peach', key: '5' },
-    { name: 'toad', key: '6' },
-    { name: 'bowser', key: '7' },
+  const [listTask, setListTask] = useState([
+    { task: "Makan mie", key: "1" },
+    { task: "Project", key: "2" },
+    { task: "Blender (Kampus)", key: "3" },
   ]);
-
-  const handleClick = (id) => {
-    //? Log `id` ke konsol
-    console.log(id);
-  
-    //? Menggunakan setPeople untuk memperbarui state `people`
-    setPeople(prevPeople => 
-      // Mengembalikan array baru dengan menggunakan filter untuk menyaring elemen yang memiliki `key` tidak sama dengan `id` yang diterima
-      prevPeople.filter(person => person.key !== id)
-    );
-  };
-  
 
   return (
     <View style={styles.container}>
-      <View style={styles.inputContainer}>
-        <Text style={styles.headline}>Touchable Components</Text>
+      {/* Header */}
+      <Header />
+      <View style={styles.content}>
+        {/* List */}
+        <View style={styles.listContainer}>
+          <FlatList 
+            data={listTask}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                <Text style={styles.text}>{item.task}</Text>
+              </View>
+            )}
+          />
+        </View>
       </View>
-
-      <FlatList
-        numColumns={2}
-        style={styles.listContainer}
-        keyExtractor={(item) => item.key}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.item} onPress={() => handleClick(item.key)}>
-            <Text style={styles.text}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
     </View>
   );
 }
@@ -48,33 +33,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    paddingHorizontal: 16,
-    paddingTop: 20,
+    backgroundColor: "#fff",
   },
-  headline: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 16,
-  },
-  inputContainer: {
-    marginBottom: 16,
+  content: {
+    flex: 1, // Untuk memastikan konten mengisi seluruh ruang yang tersedia
   },
   listContainer: {
     marginTop: 16,
+    flex: 1, // Memastikan FlatList mengisi seluruh ruang yang tersedia di dalam content
   },
   item: {
-    flex: 1,
     margin: 8,
     padding: 20,
-    backgroundColor: 'aqua',
+    backgroundColor: "aqua",
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   text: {
     fontSize: 18,
-    textAlign: 'center',
+    textAlign: "center",
   },
 });
